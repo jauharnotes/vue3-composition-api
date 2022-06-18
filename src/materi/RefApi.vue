6b1eaf73e2baf4f2bd42812fa00d31b23a551358
jauharnotes <jauharnotes@gmail.com>
2022-06-18 19:31:46 +0700

    setter and getter

---
+++ src/materi/RefApi.vue
@@ -0,0 +1,20 @@
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    const message = ref('hello');
+
+    setTimeout(() => {
+      message.value = 'nuwun';
+    }, 2000);
+
+    return {
+      message,
+    };
+  },
+};
+</script>
+
+<template>
+  <h2>{{ message }}</h2>
+</template>
