b81109f84518b7660f74a411b71433cd176885e6
jauharnotes <jauharnotes@gmail.com>
2022-06-18 11:13:40 +0700

    counter app

--- src/materi/CounterApp.vue
+++ src/materi/CounterApp.vue
@@ -28,14 +28,3 @@ export default {
   <button @click="add">ADD</button>
   <button @click="min">MIN</button>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
