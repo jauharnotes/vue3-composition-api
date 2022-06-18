63d40aaac03921b8b227651d8fdb16767cffa361
jauharnotes <jauharnotes@gmail.com>
2022-06-18 10:32:06 +0700

    Reactive API

--- src/materi/ReactiveApi.vue
+++ src/materi/ReactiveApi.vue
@@ -25,14 +25,3 @@ export default {
     <li>Age: {{ user.age }}</li>
   </ol>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
